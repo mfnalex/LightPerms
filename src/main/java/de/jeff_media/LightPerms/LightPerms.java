ba67058baf3cad2521aa8a998909032c364eaa17
mfnalex <1122571+mfnalex@users.noreply.github.com>
2021-12-02 10:07:15 +0100

    2.1.0 release

--- src/main/java/de/jeff_media/LightPerms/LightPerms.java
+++ src/main/java/de/jeff_media/LightPerms/LightPerms.java
@@ -1,16 +1,20 @@
 package de.jeff_media.LightPerms;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 import java.util.UUID;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-import de.jeff_media.PluginUpdateChecker.PluginUpdateChecker;
+import de.jeff_media.updatechecker.UpdateChecker;
 import org.apache.commons.lang.math.NumberUtils;
 import org.bstats.bukkit.Metrics;
 import org.bukkit.Bukkit;
 import org.bukkit.command.CommandExecutor;
+import org.bukkit.configuration.InvalidConfigurationException;
+import org.bukkit.configuration.file.YamlConfiguration;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.HandlerList;
@@ -24,12 +28,12 @@ import org.bukkit.plugin.java.JavaPlugin;
 public class LightPerms extends JavaPlugin implements Listener, CommandExecutor {
 
     HashMap<UUID, PermissionAttachment> perms;
-    PluginUpdateChecker updateChecker;
+    YamlConfiguration updateCheckerYaml = new YamlConfiguration();
 
     public void onEnable() {
         saveDefaultConfig();
-        updateChecker = new PluginUpdateChecker(this, "https://api.jeff-media.de/lightperms/latest-version.txt", "https://www.spigotmc.org/resources/1-8-1-16-lightperms.62447/", null, "https://chestsort.de/donate");
-        updateChecker.check(4 * 60 * 60);
+        initUpdateChecker();
+
         perms = new HashMap<>();
         getServer().getPluginManager().registerEvents(this, this);
         getCommand("lp").setTabCompleter(new TabCompleter());
@@ -39,6 +43,31 @@ public class LightPerms extends JavaPlugin implements Listener, CommandExecutor 
 
         @SuppressWarnings("unused")
         Metrics metrics = new Metrics(this, 3575);
+    }
+
+    private void initUpdateChecker() {
+        UpdateChecker.init(this,"https://api.jeff-media.de/lightperms/latest-version.txt")
+                .setDownloadLink("https://www.spigotmc.org/resources/1-8-1-16-lightperms.62447/")
+                .setChangelogLink(62447)
+                .setDonationLink("https://paypal.me/mfnalex")
+                .suppressUpToDateMessage(true);
+
+        File updateCheckerFile = new File(getDataFolder(),"updatechecker.yml");
+        if(!updateCheckerFile.exists()) {
+            saveResource("updatechecker.yml",true);
+        }
+        try {
+            updateCheckerYaml.load(updateCheckerFile);
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (InvalidConfigurationException e) {
+            e.printStackTrace();
+        }
+        if(updateCheckerYaml.getString("check-for-updates","true").equalsIgnoreCase("true")) {
+            UpdateChecker.getInstance().checkNow().checkEveryXHours(updateCheckerYaml.getDouble("check-interval",4));
+        } else if(updateCheckerYaml.getString("check-for-updates","true").equalsIgnoreCase("on-startup")) {
+            UpdateChecker.getInstance().checkNow();
+        }
     }
 
     // Returns 16 for 1.16, etc.
@@ -104,7 +133,6 @@ public class LightPerms extends JavaPlugin implements Listener, CommandExecutor 
     }
 
     public void onDisable() {
-        updateChecker.stop();
         removePermissions();
         saveConfig();
         HandlerList.unregisterAll((Listener) this);
